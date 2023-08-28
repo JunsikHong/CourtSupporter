589a198c91b26b0bb3f130a069dae6429d41717b
pyeon317 <pyun317@gmail.com>
2023-08-28 19:58:48 +0900

    20230828 css html put in sts Jinung01

--- src/main/java/com/court/supporter/controller/MainController.java
+++ src/main/java/com/court/supporter/controller/MainController.java
@@ -10,4 +10,46 @@ public class MainController {
 	public String main() {
 		return "/main";
 	}
+	
+	@GetMapping("/noticeList")
+	public String noticeList() {
+		return "notice/noticeList";
+	}
+	
+	@GetMapping("/noticeDetail")
+	public String noticeDetail() {
+		return "notice/noticeDetail";
+	}
+	
+	@GetMapping("/noticeModify")
+	public String noticeModify() {
+		return "notice/noticeModify";
+	}
+	
+	@GetMapping("/noticeRegist")
+	public String noticeRegist() {
+		return "notice/noticeRegist";
+	}
+	
+	@GetMapping("/faqList")
+	public String faqList() {
+		return "notice/faqList";
+	}
+	
+	@GetMapping("/faqDetail")
+	public String faqDetail() {
+		return "notice/faqDetail";
+	}
+	
+	@GetMapping("/faqModify")
+	public String faqModify() {
+		return "notice/faqModify";
+	}
+	
+	@GetMapping("/faqRegist")
+	public String faqRegist() {
+		return "notice/faqRegist";
+	}
+	
+	
 }
