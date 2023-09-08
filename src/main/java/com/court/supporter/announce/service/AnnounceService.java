d51065858d48ec10fcdd2f5e5a53bf4fb04fa8c2
jin <cestlavie0120@gmail.com>
2023-09-08 15:56:38 +0900

    modify, delete, search, print, webEditor, fileupload and download, css

--- src/main/java/com/court/supporter/announce/service/AnnounceService.java
+++ src/main/java/com/court/supporter/announce/service/AnnounceService.java
@@ -4,32 +4,37 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.ibatis.annotations.Param;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.court.supporter.command.TB_002VO;
+import com.court.supporter.command.TB_017VO;
 import com.court.supporter.util.Criteria;
 
  public interface AnnounceService {
 
 	//공고 등록
-	 public int announceRegist(TB_002VO vo); 	
-//	 public void createAnnouncemen(TB_002VO vo); //공고등록시 게시 날짜
-//	 public void updateAnnouncement(TB_002VO vo); //공고등록 후 수정 날짜
-//	
+	 public int announceRegist(TB_002VO vo, List<String> list ); //, List<MultipartFile> list 	
+
+	 
 	 //공고 목록 조회	
 	 public ArrayList<TB_002VO> announce_getList(@Param("cri") Criteria cri); //@Param("cri")
 	 public int announce_getTotal(@Param("cri") Criteria cri); 
-
-	 //공고 검색
-//	 public List<TB_002VO> searchList(String keyword, Criteria cri);
-//	 int getTotalByKeyword(String writer, String keyword);
-	
 	 
-	//공고 상세(게시글)	
+	 //공고 상세(게시글)	
 	 public TB_002VO getDetail(int announce_proper_num); 
+	 public List<TB_017VO> getFileDetail(int announce_proper_num );
+	 
+	 public TB_002VO getPrev(int announce_proper_num) ;
+	 public TB_002VO getNext(int announce_proper_num) ;
 	 
 	 public int announceModify(TB_002VO vo); //공고 수정
 	 
 	 public void announceDelete(int announce_proper_num); //공고 삭제
+//	 public void updateAnnounceNum(int announce_proper_num); //공고 삭제시 글번호 업데이트
 	
 }
--- src/main/java/com/court/supporter/controller/AnnounceController.java
+++ src/main/java/com/court/supporter/controller/AnnounceController.java
@@ -1,30 +1,45 @@
 package com.court.supporter.controller;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.court.supporter.announce.service.AnnounceService;
+import com.court.supporter.aws.service.AnnounceFileService;
 import com.court.supporter.command.TB_002VO;
+import com.court.supporter.command.TB_017VO;
 import com.court.supporter.util.Criteria;
 import com.court.supporter.util.PageVO;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 @Controller
 @RequestMapping("/announce")
@@ -34,6 +49,18 @@ public class AnnounceController {
 	@Autowired
 	@Qualifier("announceService")
 	private AnnounceService announceService;
+	
+    @Autowired
+    @Qualifier("announceFileService")
+    private AnnounceFileService announceFileService;
+
+    @Autowired
+    private S3Client s3;
+    
+    @Value("${aws_bucket_name}")
+    private String aws_bucket_name;
+
+    private String uploadPath = "announce/" + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
 	
 	//공고 등록 화면으로 이동
@@ -44,71 +71,109 @@ public class AnnounceController {
 
 	//등록시 목록으로 이동
 	@PostMapping("/announceRegForm")
-	public String regist(TB_002VO vo) {			
-		System.out.println(vo.getAnnounce_start_date());		
-		System.out.println(vo.getAnnounce_end_date());
-		System.out.println(vo.toString());
-		
+	public String regist(TB_002VO vo, RedirectAttributes ra , @RequestParam("file") List<MultipartFile> list) { //@RequestParam("announce_file") List<MultipartFile> list    MultipartFile file
+
+				
 		//임의로 관리자 및 재판조력자 고유번호 설정
 		vo.setAdmin_proper_num(1);
-		vo.setTrial_fcltt_proper_num(23);
+		vo.setTrial_fcltt_proper_num(23);		
+
 		
-		announceService.announceRegist(vo);
+		list = list.stream().filter(t -> t.isEmpty() == false).toList();
+
+	      for (MultipartFile file : list) {
+	         if (file.getContentType().contains("image") == false &&
+	            file.getContentType().contains("application/pdf") == false &&
+	            file.getContentType().contains("text/plain") == false &&
+	            file.getContentType().contains("application/x-hwp") == false) {
+	            
+	            //허용되지 않는 MIME타입인 경우 처리            
+	            ra.addFlashAttribute("msg", "올바른 파일 형식이 아닙니다");	
+	            return "redirect:/announce/announceList";
+	         }	         
+	      }	  	      
+	      
+	      List<String> fileList = announceFileService.announceRegist(list);
+	      
+	      announceService.announceRegist(vo, fileList);			
+
+	      
 		return "redirect:/announce/announceList";
 	}
 	
+	
 	//공고 목록 게시판 조회 + 검색
 	@GetMapping("/announceList")
-	public String announceList(Model model, Criteria cri) { //String keyword
-	
-		//검색어 설정
-//		cri.setSearchLocation(keyword);
+	public String announceList( Model model, Criteria cri) { 
 		
 		List<TB_002VO> list = announceService.announce_getList(cri);				
 		int total = announceService.announce_getTotal(cri);
 		PageVO pageVO = new PageVO(cri, total);
+		
+		//날짜 비교		
+		for(TB_002VO vo : list) {
+			String endDate = vo.getAnnounce_end_date();
+			DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate localEndDate = LocalDate.parse(endDate, dateformat); //LocalDate
+			LocalDate today = LocalDate.now(); //LocalDate
+			if(today.isAfter(localEndDate)) {
+				vo.setAnnounceStatus("모집완료");
+			} else {
+				vo.setAnnounceStatus("모집중");
+			}			
+		}
+		
 		model.addAttribute("list", list);
 		model.addAttribute("pageVO", pageVO);   
-		
 
-		//System.out.println(model.toString());
-		
 		return "announce/announceList";
 	}
 	
 	
 	//공고 게시글(공고 상세 화면)
-	@GetMapping("/announceDetail")
-	public String announceDetail(@RequestParam("announce_proper_num") int announce_proper_num, Model model) {
+	@GetMapping("/announceDetail") //파일첨부기능도 떠야함
+	public String announceDetail(@RequestParam("announce_proper_num") int announce_proper_num, Model model) { //@RequestParam("announce_file_proper_num") int announce_file_proper_num)
+//		fileVO.setAnnounce_proper_num(123);
 		
 		TB_002VO vo = announceService.getDetail(announce_proper_num);
-		model.addAttribute("vo", vo);
-		//System.out.println(vo.toString());
 		
+		List<TB_017VO> filevo = announceService.getFileDetail(announce_proper_num);
+		
+		
+		 // 이전 글과 다음 글 조회
+	    TB_002VO previousPost = announceService.getPrev(announce_proper_num);
+	    TB_002VO nextPost = announceService.getNext(announce_proper_num);
+	    
+	    model.addAttribute("filevo", filevo);
+	    model.addAttribute("previousPost", previousPost);
+	    model.addAttribute("nextPost", nextPost);
+	    model.addAttribute("vo", vo);
+				
 		return "announce/announceDetail";
 	}
 	
+	
 	//공고 수정 화면
 	@GetMapping("/announceModify")
-	public String announceModify(/* @RequestParam("announce_proper_num") int announce_proper_num, Model model */) {
-		/*
-		 * TB_002VO vo = announceService.getDetail(announce_proper_num);
-		 * model.addAttribute("vo", vo); System.out.println(vo.toString());
-		 */
+	public String announceModify(@RequestParam("announce_proper_num") int announce_proper_num, Model model) {
+		
+		 TB_002VO vo = announceService.getDetail(announce_proper_num);
+		 model.addAttribute("vo", vo); System.out.println(vo.toString());
+		
 		return "announce/announceModify";
 	}
 	
 	//공고 수정후 게시물로 이동
-	@PostMapping("/modifyForm")
-	public String modify(TB_002VO vo, RedirectAttributes ra) {
+	@PostMapping("/announceModifyForm")
+	public String modify(@ModelAttribute("vo") TB_002VO vo, RedirectAttributes ra) {
 		
 		System.out.println(vo); //확인
 		
 		int result = announceService.announceModify(vo);
 		String msg = result == 1 ? "수정 완료" : "수정 실패";
 		ra.addFlashAttribute("msg", msg);
+		return "redirect:/announce/announceList";
 		
-		return "redirect:/announce/announceDetail"; //or List
 	}
 	
 	
@@ -116,7 +181,7 @@ public class AnnounceController {
 	@PostMapping("/deleteForm")
 	public String deleteForm(@RequestParam("announce_proper_num") int announce_proper_num, RedirectAttributes ra) {
 		
-		//announceService.announceDelete(announce_proper_num);
+		announceService.announceDelete(announce_proper_num);
 		ra.addFlashAttribute("deleteForm", "삭제하시겠습니까?");
 		
 		return "redirect:/announce/announceList";
