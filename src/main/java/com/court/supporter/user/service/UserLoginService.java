506860e434bb1d31c807c1749d20b428897c09b0
hhh-one <904lhw@naver.com>
2023-09-15 15:57:16 +0900

    ignore file 적용

--- src/main/java/com/court/supporter/user/service/UserLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.court.supporter.user.service;
-
-import com.court.supporter.command.TB_018VO;
-import com.court.supporter.security.jwt.JwtToken;
-
-public interface UserLoginService {
-	//로그인
-	JwtToken login(TB_018VO tb_018vo);
-}
