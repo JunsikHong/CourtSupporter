61a781d451fdb0ba7e45c9609ce3bc16c60cf406
JunsikHong <ghdwnstlr1188@gmail.com>
2023-09-22 14:56:39 +0900

    totalsearch

---
+++ src/main/java/com/court/supporter/main/service/MainMapper.java
@@ -0,0 +1,21 @@
+package com.court.supporter.main.service;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.court.supporter.command.TB_002VO;
+import com.court.supporter.command.TB_003VO;
+import com.court.supporter.command.TB_004VO;
+
+@Mapper
+public interface MainMapper {
+
+	public ArrayList<TB_002VO> getannouncelist(String search);
+	public ArrayList<TB_003VO> getnoticelist(String search);
+	public ArrayList<TB_004VO> getfaqlist(String search);
+	public int getannouncetotal(String search);
+	public int getnoticetotal(String search);
+	public int getfaqtotal(String search);
+
+}
