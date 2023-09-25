68a5ebc03440cf639b4e8d6a23d3eb67f0b5f8d8
hhh-one <904lhw@naver.com>
2023-09-25 17:39:08 +0900

    chore: session 저장

--- src/main/java/com/court/supporter/controller/MainController.java
+++ src/main/java/com/court/supporter/controller/MainController.java
@@ -39,7 +39,6 @@ public class MainController {
   @Qualifier("mainService")
   private MainService mainService;
 
-
   @GetMapping("/")
   public String main(@CookieValue(name = "Authorization", required = false) String token,
       HttpServletRequest request, Model model) {
@@ -52,6 +51,7 @@ public class MainController {
       DefaultUserDetails userDetails = (DefaultUserDetails) authentication.getPrincipal();
 
       List<GrantedAuthority> member_role = (List<GrantedAuthority>) userDetails.getAuthorities();
+      session.setAttribute("member_role", member_role.get(0).getAuthority());
     }
 
     ArrayList<TB_002VO> tb_002list = mainService.getannouncelist("");
@@ -94,22 +94,23 @@ public class MainController {
     ArrayList<TB_004VO> tb_004list = mainService.getfaqlist(search);
 
     int tb_002total = mainService.getannouncetotal(search);
-	int tb_003total = mainService.getnoticetotal(search);
-	int tb_004total = mainService.getfaqtotal(search);
-	
-	for (int i = 0; i < tb_002list.size(); i++) {
-	    TB_002VO tb_002 = tb_002list.get(i);
-	    String cleanedData = tb_002.getAnnounce_content().replaceAll("\\<.*?\\>", "").replaceAll("&nbsp;", "").replaceAll("-", "");
-	    tb_002.setAnnounce_content(cleanedData);
-	}
-	
-	model.addAttribute("tb_002list", tb_002list);
-	model.addAttribute("tb_003list", tb_003list);
-	model.addAttribute("tb_004list", tb_004list);
-	model.addAttribute("tb_002total", tb_002total);
-	model.addAttribute("tb_003total", tb_003total);
-	model.addAttribute("tb_004total", tb_004total);
-	model.addAttribute("search", search);
+    int tb_003total = mainService.getnoticetotal(search);
+    int tb_004total = mainService.getfaqtotal(search);
+
+    for (int i = 0; i < tb_002list.size(); i++) {
+      TB_002VO tb_002 = tb_002list.get(i);
+      String cleanedData = tb_002.getAnnounce_content().replaceAll("\\<.*?\\>", "").replaceAll("&nbsp;", "")
+          .replaceAll("-", "");
+      tb_002.setAnnounce_content(cleanedData);
+    }
+
+    model.addAttribute("tb_002list", tb_002list);
+    model.addAttribute("tb_003list", tb_003list);
+    model.addAttribute("tb_004list", tb_004list);
+    model.addAttribute("tb_002total", tb_002total);
+    model.addAttribute("tb_003total", tb_003total);
+    model.addAttribute("tb_004total", tb_004total);
+    model.addAttribute("search", search);
 
     return "total-search";
   }
